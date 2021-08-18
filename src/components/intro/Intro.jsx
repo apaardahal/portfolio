838b3230b482239f8ed291eeedb35ef69f0acd57
apaardahal <cullaapar@gmail.com>
2021-08-18 23:14:10 +0545

    Intro completed
    Intro component completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,36 @@
 import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+             showCursor: true,
+             backDelay: 1500,
+             backSpeed: 60,
+              strings: ['MERN STACK DEVELOPER', 'FREELANCER', 'BLOGGER' ] })
+    },[])
+
     return (
-        <div className = "intro">
+        <div className = "intro"  id = "intro">
+            <div className = "left">
+                <div className = "imgContainer">
+                    <img src = "assets/mainimg.png"/>
+                </div>
+            </div>
+            <div className = "right">
+                <div className = "wrapper">
+                    <h2>Hello, I'm </h2>
+                    <h1> Apaar Dahal</h1>
+                    <h3> <span ref = {textRef}></span></h3>
+                </div>
+                <a href = "#portfolio">
+                    <img src = "assets/downarrow.png"></img>
+                </a>
+            </div>
         </div>
     )
 }
